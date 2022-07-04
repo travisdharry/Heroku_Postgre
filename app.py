c0bc3e72857bfb07d2dce4dc2df347226b41d739
Travis <travisdharry@gmail.com>
2022-07-04 15:17:10 -0500

    write sql query

--- app.py
+++ app.py
@@ -2,12 +2,25 @@
 # Flask converts Python outputs to a format browsers can read
 from flask import Flask, render_template, request
 # Psycopg2 allows Python to interface with PostgreSQL databases
-# import psycopg2
+import psycopg2
+import pandas as pd
+import os
 
 # Create a new Flask instance
 app = Flask(__name__)
 
+# Find environment variables
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+
 # Create Flask route
 @app.route('/')
 def index():
-    return render_template("index.html")
+    con = psycopg2.connect(DATABASE_URL)
+    cur = con.cursor()
+    # query 
+    query = "SELECT * FROM player_df"
+
+    # return results as a dataframe
+    results = pd.read_sql(query, con)
+
+    return render_template("index.html", tables=[df.to_html(classes='data')], titles=df.columns.values)
---
+++ sources.md
@@ -0,0 +1,11 @@
+# Sources
+
+
+## Database in Heroku Postgre
+Heroku
+https://devcenter.heroku.com/articles/getting-started-with-python?singlepage=true
+
+Toward Data Science
+https://towardsdatascience.com/set-up-heroku-postgresql-for-your-app-in-python-7dad9ceb0f92
+
+
